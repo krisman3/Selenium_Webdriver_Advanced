32b287f536049727a92ee7441a0543580f87d95a
KIliev <kriskokz@gmail.com>
2024-07-05 14:39:50 +0300

    Added a pages class where I will put all relevant pages

--- src/test/java/com/herokuapp/theinternet/DragNDropTest.java
+++ src/test/java/com/herokuapp/theinternet/DragNDropTest.java
@@ -1,11 +1,16 @@
 package com.herokuapp.theinternet;
 
 import com.herokuapp.theinternet.pages.PagesNames;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.devtools.v85.page.Page;
 import org.openqa.selenium.interactions.Actions;
+import org.testng.Assert;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
+
+import java.util.Objects;
 
 public class DragNDropTest extends BaseClass{
 
@@ -27,6 +32,21 @@ public class DragNDropTest extends BaseClass{
     {
         Actions action = new Actions(driver);
         driver.get(PagesNames.DRAG_PAGE);
+
+        // Grab the column A + the text of the current box
+        WebElement boxA = driver.findElement(By.id("column-a"));
+        WebElement boxA_header = driver.findElement(By.cssSelector("#column-a > header"));
+        String boxA_text = boxA_header.getText();
+        Assert.assertEquals(boxA_text, "A");
+
+        // Grab the column B + the text of the current box
+        WebElement boxB = driver.findElement(By.id("column-b"));
+        WebElement boxB_header = driver.findElement(By.cssSelector("#column-b > header"));
+        String boxB_text = boxB_header.getText();
+        Assert.assertEquals(boxB_text, "B");
+
+        action.dragAndDrop(boxA, boxB);
+
 
     }
 }
