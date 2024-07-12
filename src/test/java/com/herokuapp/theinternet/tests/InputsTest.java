c7149350ebc5b6b072262cbcc2d59b74294113ff
KIliev <kriskokz@gmail.com>
2024-07-12 10:35:19 +0200

    developed the inputs case -> issue with JS lack of knowledge

--- src/test/java/com/herokuapp/theinternet/tests/InputsTest.java
+++ src/test/java/com/herokuapp/theinternet/tests/InputsTest.java
@@ -2,7 +2,10 @@ package com.herokuapp.theinternet.tests;
 
 import com.herokuapp.theinternet.pages.PagesNames;
 import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
@@ -11,13 +14,13 @@ public class InputsTest extends BaseClass{
 
     @BeforeTest
     public void setUpTest(){
-        setUp("edge", true);
+        setUp("edge", false);
     }
 
-    @AfterTest
-    public void tearDownTest(){
-        tearDown();
-    }
+//    @AfterTest
+//    public void tearDownTest(){
+//        tearDown();
+//    }
 
 
     @Test
@@ -25,14 +28,19 @@ public class InputsTest extends BaseClass{
 
         // Grab the page
         driver.get(PagesNames.INPUTS_PAGE);
-
+        Actions action = new Actions(driver);
+        JavascriptExecutor js = (JavascriptExecutor) driver;
         // Click on the number input field
         WebElement input_field = driver.findElement(By.cssSelector("div > input[type='number']"));
+        input_field.click();
 
         // Type a number (e.g. 236)
+        input_field.sendKeys("236");
 
         // Find the button UP
 
+        WebElement number_field = driver.findElement(By.cssSelector("input[type=number]"));
+        js.executeScript();
         // Click it
 
         // Verify that the number input is increased by one
