0bed463510f1bf2fb16f6f7d1423f09ba7739cbd
KIliev <kriskokz@gmail.com>
2024-07-15 15:42:51 +0200

    removed redundant elements

--- src/test/java/com/herokuapp/theinternet/tests/InputTests.java
+++ src/test/java/com/herokuapp/theinternet/tests/InputTests.java
@@ -40,14 +40,13 @@ public class InputTests extends BaseClass
         // Since these are not buttons per se - JS script should be used:
         // The getAttribute("value") method was used as there was an IMPLICITLY existing value field within the <input> element.
 
-        WebElement number_field = driver.findElement(By.cssSelector("div > input[type='number']"));
-        String number_field_num = number_field.getAttribute("value");
+        String number_field_num = input_field.getAttribute("value");
         System.out.println("The number is: " + number_field_num);
         int init_num = Integer.parseInt(number_field_num);
 
         // Exec. the script here:
-        js.executeScript("arguments[0].stepUp();", number_field);
-        js.executeScript("arguments[0].dispatchEvent(new Event('change'));", number_field);
+        js.executeScript("arguments[0].stepUp();", input_field);
+        js.executeScript("arguments[0].dispatchEvent(new Event('change'));", input_field);
 
 
         WebElement updated_number_field = driver.findElement(By.xpath("//div[@class='example']/input[@type='number']"));
@@ -66,17 +65,23 @@ public class InputTests extends BaseClass
         driver.get(PagesNames.INPUTS_PAGE);
         JavascriptExecutor js = (JavascriptExecutor) driver;
 
-        // Find the button DOWN (it's not an actual button, so we'll use a script
-        WebElement number_field = driver.findElement(By.cssSelector("div > input[type='number']"));
-        String number_field_num = number_field.getAttribute("value");
+        WebElement input_field = driver.findElement(By.cssSelector("div > input[type='number']"));
+        input_field.click();
+
+        input_field.sendKeys("236");
+
+        // Find the button DOWN (it's not an actual button, so we'll use a JS script)
+        // Initially we're getting the content of the <input> as a String, so we need to cast it into int
+
+        String number_field_num = input_field.getAttribute("value");
 
         int init_num = Integer.parseInt(number_field_num);
         js.executeScript("arguments[0].stepDown();",number_field);
         js.executeScript("arguments[0].dispatchEvent(new Event('change'));",number_field);
 
-        WebElement input_field = driver.findElement(By.cssSelector("div > input[type='number']"));
-        input_field.click();
 
+
+        // Now we update the value by initiating
         WebElement updated_number_field = driver.findElement(By.xpath("//div[@class='example']/input[@type='number']"));
         String upd_field_num = updated_number_field.getAttribute("value");
         int upd_num = Integer.parseInt(upd_field_num);
