deb97f2cccce7f357127de9c672e76212c64f061
KIliev <kriskokz@gmail.com>
2024-07-15 15:46:07 +0200

    completes the input increase and decrease

--- src/test/java/com/herokuapp/theinternet/tests/InputTests.java
+++ src/test/java/com/herokuapp/theinternet/tests/InputTests.java
@@ -5,27 +5,26 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebElement;
 import org.testng.Assert;
+import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
-public class InputTests extends BaseClass
-{
+public class InputTests extends BaseClass {
 
     @BeforeTest
-    public void setUpTest()
-    {
+    public void setUpTest() {
         setUp("chrome", false);
     }
 
-//    @AfterTest
-//    public void tearDownTest(){
-//        tearDown();
-//    }
+    @AfterTest
+    public void tearDownTest()
+    {
+        tearDown();
+    }
 
 
     @Test
-    public void inputTestUp()
-    {
+    public void inputTestUp() {
 
         // Grab the page
         driver.get(PagesNames.INPUTS_PAGE);
@@ -59,8 +58,7 @@ public class InputTests extends BaseClass
     }
 
     @Test
-    public void inputTestDown()
-    {
+    public void inputTestDown() {
 
         driver.get(PagesNames.INPUTS_PAGE);
         JavascriptExecutor js = (JavascriptExecutor) driver;
@@ -76,10 +74,8 @@ public class InputTests extends BaseClass
         String number_field_num = input_field.getAttribute("value");
 
         int init_num = Integer.parseInt(number_field_num);
-        js.executeScript("arguments[0].stepDown();",number_field);
-        js.executeScript("arguments[0].dispatchEvent(new Event('change'));",number_field);
-
-
+        js.executeScript("arguments[0].stepDown();", input_field);
+        js.executeScript("arguments[0].dispatchEvent(new Event('change'));", input_field);
 
         // Now we update the value by initiating
         WebElement updated_number_field = driver.findElement(By.xpath("//div[@class='example']/input[@type='number']"));
@@ -87,7 +83,7 @@ public class InputTests extends BaseClass
         int upd_num = Integer.parseInt(upd_field_num);
 
         // Verify that the number input is decreased by one
-        Assert.assertEquals(upd_num, (init_num + 1), "There was no change from the initial number!");
+        Assert.assertEquals(upd_num, (init_num - 1), "There was no change from the initial number!");
 
     }
 
