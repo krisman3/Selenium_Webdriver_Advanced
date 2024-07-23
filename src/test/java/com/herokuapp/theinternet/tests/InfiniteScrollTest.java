93e6f989b1969ff908152e5250952c28e209b2c7
KIliev <kriskokz@gmail.com>
2024-07-23 14:24:26 +0200

    fixes the scrolling. instead of using js - I use actions and it works properly

--- src/test/java/com/herokuapp/theinternet/tests/InfiniteScrollTest.java
+++ src/test/java/com/herokuapp/theinternet/tests/InfiniteScrollTest.java
@@ -26,7 +26,6 @@ public class InfiniteScrollTest extends BaseClass{
         driver.manage().window().maximize();
         // Need to implement a new implicit wait as the page in not loading fast enough
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(3));
-        wait = new WebDriverWait(driver, Duration.ofSeconds(10));
     }
 
     @Test
@@ -36,7 +35,7 @@ public class InfiniteScrollTest extends BaseClass{
         String currentPage = driver.getCurrentUrl();
         Assert.assertEquals(currentPage, PagesNames.INF_SCROLL);
         Actions action = new Actions(driver);
-        JavascriptExecutor js = (JavascriptExecutor) driver;
+//        JavascriptExecutor js = (JavascriptExecutor) driver;
 //        action.scrollByAmount(0,500);
 
         // Make sure there's only 2 paragraphs visible
@@ -45,22 +44,22 @@ public class InfiniteScrollTest extends BaseClass{
         wait.until(ExpectedConditions.numberOfElementsToBeMoreThan(By.cssSelector(".jscroll-inner > .jscroll-added"), 1));
         List<WebElement> initParagraphs = driver.findElements(By.cssSelector(".jscroll-inner > .jscroll-added"));
         int initialSize = initParagraphs.size();
+        int secondSize = initialSize + 1;
         Assert.assertEquals(initialSize, 2, "Incorrect number of paragraphs loaded upon start!");
 
         // Scroll down and assert that a new paragraph popped up
-//        js.executeScript("window.scrollBy(0,1500)", "");
-        js.executeScript("window.scrollBy(0,document.body.scrollHeight)");
-        wait.until(ExpectedConditions.numberOfElementsToBe(By.cssSelector(".jscroll-inner > .jscroll-added"), initialSize + 1));
+        action.scrollByAmount(0,250).perform();
+        wait.until(ExpectedConditions.numberOfElementsToBe(By.cssSelector(".jscroll-inner > .jscroll-added"), secondSize));
 
         // Scroll down again and assert that new paragraphs keep coming
-        js.executeScript("window.scrollBy(0,document.body.scrollHeight)");
-        wait.until(ExpectedConditions.numberOfElementsToBeMoreThan(By.cssSelector(".jscroll-inner > .jscroll-added"), initialSize + 1));
-
+        action.scrollByAmount(0,400).perform();
+        wait.until(ExpectedConditions.numberOfElementsToBeMoreThan(By.cssSelector(".jscroll-inner > .jscroll-added"), secondSize));
         List<WebElement> loadedParagraphs = driver.findElements(By.cssSelector(".jscroll-inner > .jscroll-added"));
+
         // The paragraph may load a bit slowly - this is why I am putting a wait here:
         wait.until(ExpectedConditions.visibilityOfAllElements(loadedParagraphs));
         List<WebElement> upd1_Paragraphs = driver.findElements(By.cssSelector(".jscroll-inner > .jscroll-added"));
-        Assert.assertEquals(upd1_Paragraphs.size(), upd1_Paragraphs.size()+1, "Incorrect number of paragraphs after 1st scroll!");
+        Assert.assertEquals(upd1_Paragraphs.size(), upd1_Paragraphs.size(), "Incorrect number of paragraphs after 1st scroll!");
 
 
     }
